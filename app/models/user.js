396b45a80c1aad58ab4ee227026dad25ccf19816
Justin Olson <justin.wayne.olson@gmail.com>
2017-05-01 01:29:59 +0000

    Login Screen with 4 way user authentication

---
+++ app/models/user.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var bcrypt   = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+
+    account            : {
+        email        : String,
+        password     : String,
+        facebook_id  : String,
+        facebook_token: String,
+        name         : String,
+        twitter_id   : String,
+        twitter_username: String,
+        twitter_token:  String,
+        google_id    : String,
+        google_token: String
+    },
+});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.account.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
